8b03c580437f8b1a43c1aefe6c05e57dcd11bc8f
CodingBeast-1113 <127874682+CodingBeast-1113@users.noreply.github.com>
2023-06-25 14:48:33 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
 
 
 
@@ -12,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" >
-      <body className={inter.className}>
+      <body >
 
         {children}
         </body>
